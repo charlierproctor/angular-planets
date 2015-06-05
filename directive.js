700f4bf117132d231d6b407b520ce4f827dfbdd0
Charlie Proctor <charlie@charlieproctor.com>
2015-06-04 22:45:59 -0400

    one planet orbits a center

--- directive.js
+++ directive.js
@@ -4,21 +4,33 @@ angular.module('charlierproctor.angular-planets', []).
   		var scene = new THREE.Scene();
 		var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 		var renderer = new THREE.WebGLRenderer();
+		
+		camera.position.y = -100;
+		camera.position.z = 100;
+		camera.lookAt(new THREE.Vector3(0,0,0))
 
 		var geometry = new THREE.SphereGeometry( 10, 32, 32 );
-		var material = new THREE.MeshBasicMaterial();
-		var sphere = new THREE.Mesh( geometry, material );
-		sphere.position.set(0, 0, 0)
-		scene.add( sphere );
+		var material = new THREE.MeshDepthMaterial();
+		var center = new THREE.Mesh( geometry, material );
+		center.position.set(0, 0, 0)
+		scene.add( center );
 
-		camera.position.z = 100;
+		var geometry = new THREE.SphereGeometry( 10, 32, 32 );
+		var material = new THREE.MeshDepthMaterial();
+		var sphere = new THREE.Mesh( geometry, material );
+		scene.add( sphere );
 
 		renderer.setSize( window.innerWidth, window.innerHeight );
 		element.append(renderer.domElement)
 
+		var t = 0;
 		function render() {
 			requestAnimationFrame( render );
+
+			sphere.position.set(50*Math.cos(t/100), 50*Math.sin(t/100), 0)
+
 			renderer.render( scene, camera );
+			t++;
 		}
 		render();
   	}
