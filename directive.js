c5024ce2018746124c43e354d81fa7f3446ebd92
Charlie Proctor <charlie@charlieproctor.com>
2015-06-04 21:17:11 -0400

    hooking up three.js

--- directive.js
+++ directive.js
@@ -1,7 +1,11 @@
 angular.module('charlierproctor.angular-planets', []).
   directive('ngPlanets', function () {
   	function link(scope, element, attrs){
-  		console.log("hi there")
+  		var scene = new THREE.Scene();
+		var camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+		var renderer = new THREE.WebGLRenderer();
+		renderer.setSize( window.innerWidth, window.innerHeight );
+		element.append(renderer.domElement)
   	}
     return {
     	restrict: 'E',
